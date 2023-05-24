c9fcb04f3c82ddf52385d2f91ce963ce72aa01e0
LuizOctavioSB <luizoctavios14@gmail.com>
2023-05-24 02:57:13 -0300

    Iniciado o site de jogos.

---
+++ Games.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+  <link rel="stylesheet" href="Style.css">
+  <title>IndieVerse</title>
+</head>
+
+<body>
+<header class="header">
+    <h1>IndieVerse</h1>
+    <nav>
+      <ul>
+        <li><a href="Index.php">Início</a></li>
+        <li><a href="Games.php">Catálogo</a></li>
+        <li><a href="#">Sobre Nós</a></li>
+        <li><a href="#">Contato</a></li>
+      </ul>
+    </nav>
+  </header>
+
+  <main>
+    <section class="catalog">
+      <div class="container">
+        <h2>Catálogo de Jogos</h2>
+        <div class="game-list">
+          <div class="game-item">
+            <img src="caminho/para/imagem-jogo-1.jpg" alt="Jogo 1">
+            <h3>Jogo 1</h3>
+            <p>Descrição do jogo 1.</p>
+            <a class="btn btn-primary" href="#">Saiba Mais</a>
+          </div>
+          <div class="game-item">
+            <img src="caminho/para/imagem-jogo-2.jpg" alt="Jogo 2">
+            <h3>Jogo 2</h3>
+            <p>Descrição do jogo 2.</p>
+            <a class="btn btn-primary" href="#">Saiba Mais</a>
+          </div>
+          <!-- Adicione mais itens de jogos aqui -->
+        </div>
+      </div>
+    </section>
+  </main>
+  <!-- Restante do conteúdo do seu site -->
+</body>
+</html>

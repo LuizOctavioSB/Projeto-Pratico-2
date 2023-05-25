5e61e05affaadaca4c36b8285c5bd4c56d5fed55
LuizOctavioSB <luizoctavios14@gmail.com>
2023-05-25 02:36:17 -0300

    Iniciado a construção do site, header praticamente pronta, necessita corrigir alguns detalhes. Ainda é necessário construir o corpo do site.

--- Index.php
+++ Index.php
@@ -1,80 +1,32 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
-
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-  <link rel="stylesheet" href="Estilo/Style.css">
-  <title>Bem-vindo(a) ao IndieVerse</title>
+  <link rel="stylesheet" href="Estilo/style.css">
+  <title>Silksong</title>
 </head>
 
 <body>
-  <header class="header">
-    <h1>IndieVerse</h1>
-    <nav>
-      <ul>
-        <li><a href="Index.php">Início</a></li>
-        <li><a href="Games.php">Catálogo</a></li>
-        <li><a href="#">Sobre Nós</a></li>
-        <li><a href="#">Contato</a></li>
-      </ul>
-    </nav>
+  <header id="nav">
+    <div class="logo">
+      <a href="Index.php">
+        <img src="Midias/logo.png" alt="Silksong" href="Index.php">
+        Silksong
+      </a>
+    </div>
+    <ul class="Menu">
+      <li><a href="Index.php">Home</a></li>
+      <li><a href="Index.php">Pesquisar</a></li>
+      <li><a href="Index.php">Playlists</a></li>
+      <li><a href="Index.php">Contato</a></li>
+    </ul>
+
+    <div class="usuario">
+      <a href="">Log In</a>
+      <a href="">Sign Up</a>
+    </div>
   </header>
-
-  <main>
-    <section class="hero">
-      <div class="container">
-        <h2>Bem-vindo ao IndieVerse</h2>
-        <p>A melhor seleção de jogos indie e AAA para os verdadeiros entusiastas de games!</p>
-        <a class="btn btn-primary cta-button" href="#">Explorar Agora</a>
-      </div>
-    </section>
-
-    <section>
-      <div class="card mb-3" style="max-width: 540px;">
-        <div class="row g-0">
-          <div class="col-md-4">
-            <img src="..." class="img-fluid rounded-start" alt="...">
-          </div>
-          <div class="col-md-8">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-              <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-    <h1>Lorem, ipsum dolor s<br>it amet conse<br>ctetur adipisicing el<br>it. Place<br>at iure tempore voluptatibus quo nisi magni ma<br>iores veniam repel<br>lat iste reiciendis maxime asperiores quaerat, eum an<br>imi rerum delectus, odit a u<br>nde.</h1>
-  </main>
-
-  <footer class="footer">
-  <div class="container">
-    <div class="row">
-      <div class="col-md-6">
-        <h3>Contato</h3>
-        <p>Email: info@indieverse.com</p>
-        <p>Telefone: (00) 1234-5678</p>
-      </div>
-      <div class="col-md-6">
-        <h3>Siga-nos</h3>
-        <ul class="social-media">
-          <li><a href="#"><i class="fab fa-facebook"></i></a></li>
-          <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-          <li><a href="#"><i class="fab fa-instagram"></i></a></li>
-        </ul>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-md-12">
-        <p class="text-center">© 2023 IndieVerse. Todos os direitos reservados.</p>
-      </div>
-    </div>
-  </div>
-</footer>
 </body>
-
 </html>
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Projeto-Pratico-2
+
+Decidi por fazer uma "comunidade de música", a ideia é um site que seja uma "comunidade" onde os membros possam compartilhar suas playlists, recomendações de álbuns, descobrir novos artistas, ler notícias e participar de discussões sobre música.
